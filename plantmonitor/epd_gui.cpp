640e01ebbba9261b42e8913564c3b8f59dd3cb61
Ryker Kollmyer <rykerkollmyer@gmail.com>
2026-01-06 22:02:23 -0800

    d

--- plantmonitor/epd_gui.cpp
+++ plantmonitor/epd_gui.cpp
@@ -171,8 +171,8 @@ void Gui_SetPixel(uint16_t x, uint16_t y, uint16_t color)
     }
     else
     {
-        RWimage[Addr] = BWdata | (0x80 >> (xx % 8));  
-        BWimage[Addr] = RWdata | (0x80 >> (xx % 8));
+        BWimage[Addr] = BWdata | (0x80 >> (xx % 8));
+        RWimage[Addr] = RWdata | (0x80 >> (xx % 8));
     }
 }
 
